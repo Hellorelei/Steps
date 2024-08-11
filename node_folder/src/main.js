a70eac9c6309a53d9defa2a0ce5798c6b203a0df
Lorelei! <17865635+Hellorelei@users.noreply.github.com>
2024-08-11 18:10:45 +0200

    animations are working!

--- node_folder/src/main.js
+++ node_folder/src/main.js
@@ -3,11 +3,33 @@ import "kaplay/global";
 
 const k = kaplay()
 
-k.loadSprite("bean", "sprites/bean.png")
+///k.loadSprite("bean", "sprites/bean.png")
+k.loadAseprite(
+	"starch",
+	"sprites/starch_1.png",
+	"sprites/starch_1.json"
+)
+///k.loadSpriteAtlas(
+///	"sprites/starch_1.png",
+///	"sprites/starch_1.json",
+///)
+/// k.loadSprite("starch", "http://172.22.22.61:8082/steps/assets/sprites/starch_1.gif")
 
-k.add([
-	k.pos(120, 80),
-	k.sprite("bean"),
-])
+///k.add([
+///	k.pos(120, 80),
+///	k.sprite("bean"),
+///])
 
 k.onClick(() => k.addKaboom(k.mousePos()))
+k.onClick(
+	() => k.add([
+		k.pos(k.mousePos()),
+		k.sprite(
+			"starch",
+			{
+				anim: "idle",
+			})
+	])
+)
+
+k.onClick(() => console.log(k.mousePos()))
