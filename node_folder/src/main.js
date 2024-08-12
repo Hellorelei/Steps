093bd99e5b99286b718d9659d9c6f6b72060ee1a
Lorelei! <17865635+Hellorelei@users.noreply.github.com>
2024-08-12 12:18:27 +0200

    moving files around for tidying

--- node_folder/src/main.js
+++ node_folder/src/main.js
@@ -3,17 +3,59 @@ import "kaplay/global";
 
 const k = kaplay()
 
+let start_menu;
+let window_dimensions = k.canvas
+
+
+class SpritesLoader {
+	constructor() {
+	}
+
+	static getFont() {
+		k.loadFont("Shantell Sans", "assets/fonts/Shantell_Sans/static/ShantellSans-Medium.ttf")
+	}
+	static getAssets() {
+		fetch("assets/sprites/sprite_list.json").then(res => res.json()).then(json => console.log(json));
+	}
+
+	static loadStarch() {
+		k.loadAseprite(
+			"starch",
+			"sprites/starch_1.png",
+			"sprites/starch_1.json"
+		)
+}
+}
+
+class StartMenu {
+	constructor() {}
+
+	static setupStartMenu() {
+		start_menu = scene("start_menu", () => {
+			console.log(window_dimensions)
+			console.log(k.width)
+			k.add([
+				pos(
+					100, 100,
+				),
+				text(
+					"Steps",
+					{
+						size: 48,
+						font: "Shantell Sans",
+						align: "center",
+					})
+			])
+		})
+	}
+}
+SpritesLoader.getFont()
+SpritesLoader.getAssets()
+StartMenu.setupStartMenu()
+k.go("start_menu");
+
 ///k.loadSprite("bean", "sprites/bean.png")
-k.loadAseprite(
-	"starch",
-	"sprites/starch_1.png",
-	"sprites/starch_1.json"
-)
-///k.loadSpriteAtlas(
-///	"sprites/starch_1.png",
-///	"sprites/starch_1.json",
-///)
-/// k.loadSprite("starch", "http://172.22.22.61:8082/steps/assets/sprites/starch_1.gif")
+
 
 ///k.add([
 ///	k.pos(120, 80),
